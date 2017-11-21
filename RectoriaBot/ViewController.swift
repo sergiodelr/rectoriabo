440657a0c3951aba2558c496df4f0a10d235dc58
eventos <eventos@tedu-macbook-pro.local>
2017-11-21 15:35:01 -0600

    Initial Commit

---
+++ RectoriaBot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RectoriaBot
+//
+//  Created by eventos on 11/21/17.
+//  Copyright © 2017 eventos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
